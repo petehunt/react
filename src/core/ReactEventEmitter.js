60167e4b1ddbaa087e7ed26a211fe24bf34e0700
petehunt <floydophone@gmail.com>
2013-07-01 01:08:18 -0700

    checkpoint

--- src/core/ReactEventEmitter.js
+++ src/core/ReactEventEmitter.js
@@ -24,6 +24,7 @@ var EventListener = require('EventListener');
 var EventPluginHub = require('EventPluginHub');
 var ExecutionEnvironment = require('ExecutionEnvironment');
 var ViewportMetrics = require('ViewportMetrics');
+var ReactSerialization = require('ReactSerialization');
 
 var invariant = require('invariant');
 var isEventSupported = require('isEventSupported');
